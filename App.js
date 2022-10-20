56c0ddd9373907a456a49ef599452556a4a32570
Daniel Gregorio Reyes Alvarez <dgreyes@grupocorinsa.hn>
2022-10-20 13:11:21 -0600

    Agregando estructura principal y estilos

--- App.js
+++ App.js
@@ -1,44 +1,75 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, FlatList, StyleSheet } from 'react-native'
-import CoinItem from './components/CoinItem'
-
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  StyleSheet,
+  StatusBar,
+} from "react-native";
+import CoinItem from "./components/CoinItem";
 
 const App = () => {
+  const [coins, setCoins] = useState([]);
 
-  const [coins, setCoins] = useState([])
-
-  const loadData = async() => {
+  const loadData = async () => {
     const result = await fetch(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
     );
-    const data = await result.json()
-    setCoins(data)
-  } 
+    const data = await result.json();
+    setCoins(data);
+  };
 
   useEffect(() => {
-    loadData()
-  }, [])
+    loadData();
+  }, []);
 
   return (
     <View style={styles.container}>
-
-
-      <FlatList 
+      <StatusBar backgroundColor="#141414" />
+      <View style={styles.header}>
+        <Text style={styles.title}>CryptoMarketplace</Text>
+        <TextInput style={styles.searchInput}/>
+      </View> 
+      <FlatList
+        style={styles.list}
         data={coins}
-        renderItem={({item}) => {
-          return <CoinItem coin={item}/>
+        renderItem={({ item }) => {
+          return <CoinItem coin={item} />;
         }}
+        showsVerticalScrollIndicator={false}
       />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#141414',
-    alignItems: 'center',
-    flex: 1
+    backgroundColor: "#141414",
+    alignItems: "center",
+    flex: 1,
+  },
+  title: {
+    color: "#fff",
+    marginTop: 10,
+    fontSize: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%',
+    marginBottom: 10
+  },
+  list: {
+    width: "90%",
+  },
+  searchInput: {
+    color: "#fff",
+    borderBottomColor: "#4657CE",
+    borderBottomWidth: 2,
+    width: '40%',
+    textAlign: 'center',
   }
-})
+});
 
-export default App
+export default App;
