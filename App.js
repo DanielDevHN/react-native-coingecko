fb9a8b8cb10d3bb2b712f05e01aad0c656ee91af
Daniel Gregorio Reyes Alvarez <dgreyes@grupocorinsa.hn>
2022-10-20 11:08:09 -0600

    Agregando estilos a resultados de la API

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 
-export default function App() {
+
+const App = () => {
+
+  const [coins, setCoins] = useState([])
+
+  const loadData = async() => {
+    const result = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
+    const data = await result.json()
+    setCoins(data)
+  } 
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+
+      <FlatList 
+        data={coins}
+        renderItem={({item}) => {
+          return <Text>{item.name}</Text>
+        }}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#141414',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex: 1
+  }
+})
+
+export default App
