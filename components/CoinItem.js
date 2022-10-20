13d62ed609b740fa165ceae3d86dd3bf078e35f5
Daniel Gregorio Reyes Alvarez <dgreyes@grupocorinsa.hn>
2022-10-20 13:10:24 -0600

    Agregando estilos y estructura

--- components/CoinItem.js
+++ components/CoinItem.js
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
 
-const CoinItem = ({coin}) => {
+const CoinItem = ({ coin }) => {
   return (
-    <View>
-      <Text>{coin.name}</Text>
+    <View style={styles.containerItem}>
+      <View style={styles.coinNames}>
+        <Image style={styles.image} source={{ uri: coin.image }} />
+        <Text style={styles.text}>{coin.name}</Text>
+        <Text>{coin.symbol}</Text>
+      </View>
+      <Text style={styles.price}>{coin.current_price}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default CoinItem
+const styles = StyleSheet.create({
+  containerItem: {
+    backgroundColor: "#121212",
+    paddingTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  coinNames: {
+    flexDirection: 'row'
+  },
+  image: {
+    width: 30,
+    height: 30,
+  },
+  text: {
+    color: "#ffffff",
+  },
+  price: {
+    color: "#ffffff",
+  },
+});
+
+export default CoinItem;
